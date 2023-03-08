c5dc787d3ce05e617d908a1bb613df4d266d4307
Xu Xingliang <xuxingliang@xiaomi.com>
2023-03-08 16:21:57 +0800

    examples: add animation
    Signed-off-by: Xu Xingliang <xuxingliang@xiaomi.com>

---
+++ examples/animation.lua
@@ -0,0 +1,59 @@
+local root = lugl.Object()
+root:set { w = lugl.HOR_RES(), h = lugl.VER_RES() }
+
+-- create obj on root
+local obj = root:Object()
+
+-- create image on root and set position/img src/etc. properties.
+local img = root:Image {
+    src = "/assets/lvgl-logo.png",
+    x = 0,
+    y = 0,
+    bg_color = 0x004400,
+    pad_all = 0
+}
+
+-- change image properties.
+
+img:set {
+    src = "/assets/lvgl-logo.png",
+    align = lugl.ALIGN.CENTER,
+}
+
+-- create animation on object
+local anim = img:anim {
+    run = true,
+    start_value = 0,
+    end_value = 3600,
+    duration = 2000,
+    repeat_count = 2,
+    path = "bounce",
+    exec_cb = function(obj, value)
+        obj:set {
+            angle = value
+        }
+    end
+}
+
+lugl.Timer {
+    period = 1000,
+    cb = function(t)
+        print("stop animation in timer.")
+        t:delete()
+        anim:stop()
+        anim = nil
+        collectgarbage("collect")
+        collectgarbage("collect")
+    end
+}
+
+-- create Label on root and set its font
+local label = root:Label {
+    text = string.format("Hello %03d", 123),
+    text_font = lugl.Font("montserrat", 24, "normal"),
+    align = {
+        type = lugl.ALIGN.CENTER,
+        x_ofs = 0,
+        y_ofs = 100,
+    }
+}
