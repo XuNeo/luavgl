8f163927d0b3ba9e568dcd5d60ac48aa59813e56
Xu Xingliang <xuxingliang@xiaomi.com>
2023-03-08 17:39:23 +0800

    examples: add protected call example
    Now all lugl callback funtions are made by protected call, any crash will print the callstack on screen
    
    Signed-off-by: Xu Xingliang <xuxingliang@xiaomi.com>

---
+++ examples/protectedcall.lua
@@ -0,0 +1,36 @@
+
+local function testCrash()
+    print(nil .. "")
+end
+
+local function testCrash2()
+    testCrash()
+end
+local function testCrash3()
+    testCrash2()
+end
+local function testCrash4()
+    testCrash3()
+end
+
+local function testCrash5()
+    testCrash4()
+end
+
+local function testCrash6()
+    testCrash5()
+end
+
+lugl.Label(nil, {
+    text = "crash in 1 second.",
+    align = lugl.ALIGN.CENTER
+})
+
+lugl.Timer {
+    period = 1000,
+    cb = function(t)
+        t:delete()
+        -- crash in callback function is protected.
+        testCrash6()
+    end
+}
