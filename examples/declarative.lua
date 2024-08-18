9413c485d13ec354b32277a00c0b0c2f3c64f040
Neo Xu <neo.xu1990@gmail.com>
2024-08-18 20:16:30 +0800

    examples: add declarative example
    Signed-off-by: Neo Xu <neo.xu1990@gmail.com>

---
+++ examples/declarative.lua
@@ -0,0 +1,37 @@
+local lvgl = require("lvgl")
+
+Object {
+    flex = {
+        flex_direction = "row",
+        flex_wrap = "wrap",
+        justify_content = "center",
+        align_items = "center",
+        align_content = "center",
+    },
+    w = 400,
+    h = 100,
+    align = lvgl.ALIGN.CENTER,
+
+    -- Button with label, inside a container
+    Object {
+        w = 150,
+        h = lvgl.PCT(80),
+        bg_color = "#aa0",
+
+        Button {
+            Label {
+                text = string.format("BUTTON %d", 1),
+                align = lvgl.ALIGN.CENTER
+            }
+        }:center()
+    }:clear_flag(lvgl.FLAG.SCROLLABLE),
+
+    -- Label inside a container
+    Object {
+        w = 150,
+        h = lvgl.PCT(80),
+        Label {
+            text = string.format("label %d", 2)
+        }:center()
+    }:clear_flag(lvgl.FLAG.SCROLLABLE)
+}
